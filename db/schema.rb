136c8bda032e5aa011094912737fa936527d632c
luxedis <shinobi85@gmail.com>
2020-12-20 07:52:40 +0000

    ユーザーモデル完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201219131514) do
+ActiveRecord::Schema.define(version: 20201220071923) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

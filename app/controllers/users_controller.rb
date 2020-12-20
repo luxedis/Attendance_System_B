e479f2f0549a4f5130c7d6fe417c995451d3bf56
luxedis <shinobi85@gmail.com>
2020-12-20 13:05:51 +0000

    勤怠５ 5.4ユーザー情報ページまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(parmas[:id])
+  end
+  
   def new
+    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入する。
   end
 end

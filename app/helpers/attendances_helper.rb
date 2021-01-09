b2e76ecb95c4024b4009e547159b99e634da1ae0
luxedis <shinobi85@gmail.com>
2021-01-09 05:15:05 +0000

    10.6.1退勤ボタン実装

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,12 @@
 module AttendancesHelper
+  
+  def attendance_state(attendance)
+    # 受け取ったAttendanceオブジェクトが当日と一致するかを評価
+    if Date.current == attendance.worked_on
+      return '出勤' if attendance.started_at.nil?
+      return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
+    end
+    # どれにも該当しなかった場合はfalseを返す
+    false
+  end
 end

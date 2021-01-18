08a0dd90722f8a117874c96c0800fd312c76dee7
luxedis <shinobi85@gmail.com>
2021-01-18 13:00:07 +0000

    出勤の時間と分、修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,14 @@ module UsersHelper
   
   # 勤怠基本情報を指定のフォーマットで返す
   def format_basic_info(time)
-    format("%.2f", ((time.hour * 60) + time.min) / 60.0)
+    format("%.2f", ((time.hour * 60) + time.min) / 60.0) # %.2fで小数点以下２桁指定しても、60.0と小数点指定しないと
+  end
+  
+  def format_hour(time)
+    format("%02d", time.hour) # 時間を整数で表す
+  end
+  
+  def format_min(time)
+    format("%02d", (time.min * 15) / 15)
   end
 end

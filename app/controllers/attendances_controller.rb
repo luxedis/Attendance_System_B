ba8469bf6cecc106e2bb659463d2c64571d79fd1
luxedis <shinobi85@gmail.com>
2021-01-24 07:45:49 +0000

    update_one_monthアクション

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -29,7 +29,17 @@ class AttendancesController < ApplicationController
   end
   
   def update_one_month
-    # debugger
+    ActiveRecord::Base.transaction do # トランザクション(分割できないワンセット処理の事)の開始
+      attendances_params.each do |id, item|
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+      end
+    end
+    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+    redirect_to user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
   
   private
